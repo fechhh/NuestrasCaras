39af61633d69d28ab2dd7cf54fa2dc9debaf9e4e
Federico Grijalba <federico.grijalba@gmail.com>
2024-05-04 17:38:04 -0300

    actualizo main y perceptrones

--- perceptrones.py
+++ perceptrones.py
@@ -7,8 +7,6 @@
 
 import math
 import numpy as np
-import matplotlib.pyplot as plt
-from mlxtend.preprocessing import standardize
 
 def func_eval(fname, x):
     match fname:
@@ -37,23 +35,17 @@ def deriv_eval(fname, y):  #atencion que y es la entrada y=f( x )
             d = 1.0 - y*y
     return d
 
-def deriv_eval_vec(fname, y):  #atencion que y es la entrada y=f( x )
-    if fname == 'purelin':
-        d = np.ones(y.shape)
-    elif fname == 'logsig':
-        d = y*(1.0-y)
-    elif fname == 'tansig':
-        d = 1.0 - y*y
-    return d
-
-
 
 def backpropagation_1_capa(entrada, salida, n_neuronas, epoch_limit, Error_umbral, learning_rate):
     # Paso las listas a numpy
     X = np.array(entrada)
     Y = np.array(salida).reshape(len(X),1)
     
+    # Vectorizar la función para poder pasarle un vector para los cálculos
     func_eval_vec = np.vectorize(func_eval)
+
+    # Vectorizar la función para poder pasarle un vector para los cálculos
+    deriv_eval_vec = np.vectorize(deriv_eval)
 
     filas_qty = len(X)
     input_size = X.shape[1]   # 2 entradas
@@ -136,9 +128,13 @@ def backpropagation_1_capa(entrada, salida, n_neuronas, epoch_limit, Error_umbra
 def backpropagation_2_capas(entrada, salida, n_neuronas_capa1, n_neuronas_capa2, epoch_limit, Error_umbral, learning_rate):
     # Paso las listas a numpy
     X = np.array(entrada)
-    Y = np.array(salida).reshape(len(X),1)
+    Y = salida
     
+    # Vectorizar la función para poder pasarle un vector para los cálculos
     func_eval_vec = np.vectorize(func_eval)
+
+    # Vectorizar la función para poder pasarle un vector para los cálculos
+    deriv_eval_vec = np.vectorize(deriv_eval)
 
     filas_qty = len(X)
     input_size = X.shape[1]   # 2 entradas
@@ -149,7 +145,7 @@ def backpropagation_2_capas(entrada, salida, n_neuronas_capa1, n_neuronas_capa2,
     # defino las funciones de activacion de cada capa
     hidden_FUNC1 = 'logsig'  # uso la logistica
     hidden_FUNC2 = 'logsig'  # uso la logistica
-    output_FUNC = 'tansig'  # uso la tangente hiperbolica
+    output_FUNC = 'logsig'  # uso la tangente hiperbolica
 
 
     # Incializo las matrices de pesos azarosamente
@@ -173,12 +169,14 @@ def backpropagation_2_capas(entrada, salida, n_neuronas_capa1, n_neuronas_capa2,
     output_estimulos = W3 @ hidden_salidas2 + X03
     output_salidas = func_eval_vec(output_FUNC, output_estimulos)
     
-    # calculo el error promedi general de TODOS los X
+    # calculo el error promedio general de TODOS los X
     Error= np.mean( (Y.T - output_salidas)**2 )
+    print(f"Error inicial: {Error}")
     
     # Inicializo
     Error_last = 10    # lo debo poner algo dist a 0 la primera vez
     epoch = 0
+    # epoch_limit, Error_umbral, learning_rate son parametros de ingreso a la funcion
     
     while ( math.fabs(Error_last-Error)>Error_umbral and (epoch < epoch_limit)):
         epoch += 1
@@ -221,12 +219,15 @@ def backpropagation_2_capas(entrada, salida, n_neuronas_capa1, n_neuronas_capa2,
         
         # calculo el error promedio general de TODOS los X
         Error= np.mean( (Y.T - output_salidas)**2 )
+
+    # Imprimo epoch y error
+    print(f"Epoch: {epoch}, Error: {Error}")
         
     return W1, X01, W2, X02, W3, X03, Error, epoch
 
 
 
-def predecir_clase_1_neurona(pesos, entrada):
+def predecir_clase_1_capa(pesos, entrada):
     # Desempaquetar los pesos
     W1, X01, W2, X02 = pesos
     
@@ -250,7 +251,7 @@ def predecir_clase_1_neurona(pesos, entrada):
 
 
 
-def predecir_clase_2_neuronas(pesos, entrada):
+def predecir_clase_2_capas(pesos, entrada):
     # Desempaquetar los pesos
     W1, X01, W2, X02, W3, X03 = pesos
 
@@ -259,202 +260,20 @@ def predecir_clase_2_neuronas(pesos, entrada):
     # Definir las funciones de activación de cada capa
     hidden_FUNC1 = 'logsig'  # uso la logistica
     hidden_FUNC2 = 'logsig'  # uso la logistica
+    output_FUNC = 'logsig'  # uso la logistica
 
     func_eval_vec = np.vectorize(func_eval)
 
     # Avanzar la red, forward
-    hidden_estimulos1 = np.dot(W1, entrada.T) + X01
-    hidden_salidas1 = func_eval_vec(hidden_FUNC1, hidden_estimulos1)
-    hidden_estimulos2 = np.dot(W2, hidden_salidas1) + X02
-    hidden_salidas2 = func_eval_vec(hidden_FUNC2, hidden_estimulos2)
-    output_estimulos = np.dot(W3, hidden_salidas2) + X03
-    output_salidas = softmax(output_estimulos)
-
-    # Obtener la clase predicha
-    clase_predicha = np.argmax(output_salidas)
-
-    return output_salidas, clase_predicha
-
-
-
-def backpropagation_1_capa_multi(entrada, salida, n_neuronas_capa_oculta, epoch_limit, Error_umbral, learning_rate):
-    X = np.array(entrada)
-    Y = np.array(salida)
-
-    func_eval_vec = np.vectorize(func_eval)
-
-    filas_qty = len(X)
-    input_size = X.shape[1]
-    hidden_size = n_neuronas_capa_oculta
-    output_size = Y.shape[1]
-
-    hidden_FUNC = 'tansig'  # Use ReLU for the hidden layer
-    output_FUNC = 'tansig'
-
-    np.random.seed(1021)
-    W1 = np.random.uniform(-0.5, 0.5, [hidden_size, input_size])
-    X01 = np.random.uniform(-0.5, 0.5, [hidden_size, 1])
-    W2 = np.random.uniform(-0.5, 0.5, [output_size, hidden_size])
-    X02 = np.random.uniform(-0.5, 0.5, [output_size, 1])
-
-    hidden_estimulos = W1 @ X.T + X01
-    hidden_salidas = func_eval_vec(hidden_FUNC, hidden_estimulos)
-    output_estimulos = W2 @ hidden_salidas + X02
-    output_salidas = func_eval_vec(output_FUNC, output_estimulos)
-
-    Error = np.mean((Y.T - output_salidas) ** 2)
-
-    Error_last = 10
-    epoch = 0
-
-    while (math.fabs(Error_last - Error) > Error_umbral and (epoch < epoch_limit)):
-        epoch += 1
-        Error_last = Error
-
-        for fila in range(filas_qty):
-            hidden_estimulos = W1 @ X[fila:fila + 1, :].T + X01
-            hidden_salidas = func_eval_vec(hidden_FUNC, hidden_estimulos)
-            output_estimulos = W2 @ hidden_salidas + X02
-            output_salidas = func_eval_vec(output_FUNC, output_estimulos)
-
-            ErrorSalida = Y[fila:fila + 1, :].T - output_salidas
-            output_delta = ErrorSalida * deriv_eval_vec(output_FUNC, output_salidas)
-            hidden_delta = deriv_eval_vec(hidden_FUNC, hidden_salidas) * (W2.T @ output_delta)
-
-            W1 = W1 + learning_rate * (hidden_delta @ X[fila:fila + 1, :])
-            X01 = X01 + learning_rate * hidden_delta
-            W2 = W2 + learning_rate * (output_delta @ hidden_salidas.T)
-            X02 = X02 + learning_rate * output_delta
-
-        hidden_estimulos = W1 @ X.T + X01
-        hidden_salidas = func_eval_vec(hidden_FUNC, hidden_estimulos)
-        output_estimulos = W2 @ hidden_salidas + X02
-        output_salidas = func_eval_vec(output_FUNC, output_estimulos)
-
-        Error = np.mean((Y.T - output_salidas) ** 2)
-
-    return W1, X01, W2, X02, epoch, Error
-
-
-def predecir_clase_1_capa_multi(pesos, entrada):
-    # Desempaquetar los pesos
-    W1, X01, W2, X02 = pesos
-    
-    entrada = np.array(entrada)
-
-    # Definir las funciones de activación de cada capa
-    hidden_FUNC = 'tansig'  # Use ReLU for the hidden layer
-    output_FUNC = 'tansig'
-
-    func_eval_vec = np.vectorize(func_eval)
-
-    # Avanzar la red, forward
-    hidden_estimulos = np.dot(W1, entrada.T) + X01
-    hidden_salidas = func_eval_vec(hidden_FUNC, hidden_estimulos)
-    output_estimulos = np.dot(W2, hidden_salidas) + X02
-    output_salidas = func_eval_vec(output_FUNC, output_estimulos)
-
-    # Obtener la clase predicha
-    clase_predicha = np.argmax(output_salidas)
-
-    return output_salidas, clase_predicha
-
-
-
-def backpropagation_2_capas_multi(entrada, salida, n_neuronas_capa1, n_neuronas_capa2, epoch_limit, Error_umbral, learning_rate):
-    X = np.array(entrada)
-    Y = np.array(salida)
-
-    func_eval_vec = np.vectorize(func_eval)
-
-    filas_qty = len(X)
-    input_size = X.shape[1]
-    hidden_size1 = n_neuronas_capa1
-    hidden_size2 = n_neuronas_capa2
-    output_size = Y.shape[1]
-
-    hidden_FUNC1 = 'tansig'
-    hidden_FUNC2 = 'tansig'
-    output_FUNC = 'tansig'
-
-    np.random.seed(1021)
-    W1 = np.random.uniform(-0.5, 0.5, [hidden_size1, input_size])
-    X01 = np.random.uniform(-0.5, 0.5, [hidden_size1, 1])
-    W2 = np.random.uniform(-0.5, 0.5, [hidden_size2, hidden_size1])
-    X02 = np.random.uniform(-0.5, 0.5, [hidden_size2, 1])
-    W3 = np.random.uniform(-0.5, 0.5, [output_size, hidden_size2])
-    X03 = np.random.uniform(-0.5, 0.5, [output_size, 1])
-
-    hidden_estimulos1 = W1 @ X.T + X01
+    hidden_estimulos1 = W1 @ entrada.T + X01
     hidden_salidas1 = func_eval_vec(hidden_FUNC1, hidden_estimulos1)
     hidden_estimulos2 = W2 @ hidden_salidas1 + X02
     hidden_salidas2 = func_eval_vec(hidden_FUNC2, hidden_estimulos2)
     output_estimulos = W3 @ hidden_salidas2 + X03
     output_salidas = func_eval_vec(output_FUNC, output_estimulos)
 
-    Error = np.mean((Y.T - output_salidas) ** 2)
-
-    Error_last = 10
-    epoch = 0
-
-    while (math.fabs(Error_last - Error) > Error_umbral and (epoch < epoch_limit)):
-        epoch += 1
-        Error_last = Error
-
-        for fila in range(filas_qty):
-            hidden_estimulos1 = W1 @ X[fila:fila + 1, :].T + X01
-            hidden_salidas1 = func_eval_vec(hidden_FUNC1, hidden_estimulos1)
-            hidden_estimulos2 = W2 @ hidden_salidas1 + X02
-            hidden_salidas2 = func_eval_vec(hidden_FUNC2, hidden_estimulos2)
-            output_estimulos = W3 @ hidden_salidas2 + X03
-            output_salidas = func_eval_vec(output_FUNC, output_estimulos)
-
-            ErrorSalida = Y[fila:fila + 1, :].T - output_salidas
-            output_delta = ErrorSalida * deriv_eval_vec(output_FUNC, output_salidas)
-            hidden_delta2 = deriv_eval_vec(hidden_FUNC2, hidden_salidas2) * (W3.T @ output_delta)
-            hidden_delta1 = deriv_eval_vec(hidden_FUNC1, hidden_salidas1) * (W2.T @ hidden_delta2)
-
-            W1 = W1 + learning_rate * (hidden_delta1 @ X[fila:fila + 1, :])
-            X01 = X01 + learning_rate * hidden_delta1
-            W2 = W2 + learning_rate * (hidden_delta2 @ hidden_salidas1.T)
-            X02 = X02 + learning_rate * hidden_delta2
-            W3 = W3 + learning_rate * (output_delta @ hidden_salidas2.T)
-            X03 = X03 + learning_rate * output_delta
-
-        hidden_estimulos1 = W1 @ X.T + X01
-        hidden_salidas1 = func_eval_vec(hidden_FUNC1, hidden_estimulos1)
-        hidden_estimulos2 = W2 @ hidden_salidas1 + X02
-        hidden_salidas2 = func_eval_vec(hidden_FUNC2, hidden_estimulos2)
-        output_estimulos = W3 @ hidden_salidas2 + X03
-        output_salidas = func_eval_vec(output_FUNC, output_estimulos)
-
-        Error = np.mean((Y.T - output_salidas) ** 2)
-
-    return W1, X01, W2, X02, W3, X03, epoch, Error
-
-
-def predecir_clase_2_capas_multi(pesos, entrada):
-    # Desempaquetar los pesos
-    W1, X01, W2, X02, W3, X03 = pesos
-
-    entrada = np.array(entrada)
-
-    # Definir las funciones de activación de cada capa
-    hidden_FUNC1 = 'tansig'
-    hidden_FUNC2 = 'tansig'
-    output_FUNC = 'tansig'
-
-    func_eval_vec = np.vectorize(func_eval)
-
-    # Avanzar la red, forward
-    hidden_estimulos1 = np.dot(W1, entrada.T) + X01
-    hidden_salidas1 = func_eval_vec(hidden_FUNC1, hidden_estimulos1)
-    hidden_estimulos2 = np.dot(W2, hidden_salidas1) + X02
-    hidden_salidas2 = func_eval_vec(hidden_FUNC2, hidden_estimulos2)
-    output_estimulos = np.dot(W3, hidden_salidas2) + X03
-    output_salidas = func_eval_vec(output_FUNC, output_estimulos)
-
     # Obtener la clase predicha
     clase_predicha = np.argmax(output_salidas)
 
     return output_salidas, clase_predicha
+
